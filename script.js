486105fd8787a0b721a898b7eb793015b7ed6ed7
falidill <158863882+falidill@users.noreply.github.com>
2025-03-04 23:28:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
